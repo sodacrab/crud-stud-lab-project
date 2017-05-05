11dfde4ef79c4262afa76499ffaffab1fae03282
Iwanpetr <petrovivanvlad@yandex.ru>
2017-05-06 06:00:41 +0700

    допилил функционал. страшновато получилось, но главное - круд

--- app/src/main/java/com/shishkovictor/onlinephonebook/APIInterface.java
+++ app/src/main/java/com/shishkovictor/onlinephonebook/APIInterface.java
@@ -1,9 +1,13 @@
 package com.shishkovictor.onlinephonebook;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface APIInterface {
 
@@ -12,6 +16,12 @@ public interface APIInterface {
 
     @GET("/todo/api/v1.0/tasks")
     Call<Object> getUsers();
+
+    @PUT("/todo/api/v1.0/tasks/{task_id}")
+    Call<User> updateUserByID(@Path("task_id") int id, @Body User user);
+
+    @DELETE("/todo/api/v1.0/tasks/{task_id}")
+    Call<ResponseBody> deleteUserByID(@Path("task_id") int id);
 
 }
 
--- app/src/main/java/com/shishkovictor/onlinephonebook/MainActivity.java
+++ app/src/main/java/com/shishkovictor/onlinephonebook/MainActivity.java
@@ -1,15 +1,18 @@
 package com.shishkovictor.onlinephonebook;
 
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -17,6 +20,7 @@ import com.google.gson.GsonBuilder;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -29,7 +33,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView tv;
     private EditText et_students_name;
     private EditText et_students_phone;
+    private Button button_refresh;
     private Button button_send_post_request;
+    private EditText et_id;
+    private Button button_change;
 
     //private final String url = "http://10.0.2.2:1337"; // для дебага на компе с эмулятра
     private final String url = "http://iwanp.tintekko.com:1337";
@@ -53,22 +60,29 @@ public class MainActivity extends AppCompatActivity {
         tv.setText("");
         et_students_name = (EditText) findViewById(R.id.editText_name);
         et_students_phone = (EditText) findViewById(R.id.editText_phone);
+        button_refresh = (Button) findViewById(R.id.button2_refresh);
+        button_refresh.setOnClickListener(onClickListener);
         button_send_post_request = (Button) findViewById(R.id.button_send);
         button_send_post_request.setOnClickListener(onClickListener);
+        et_id = (EditText) findViewById(R.id.editText_id);
+        button_change = (Button) findViewById(R.id.button_change);
+        button_change.setOnClickListener(onClickListener);
 
-        // TODO: move to the doInBackground method:
-        Thread t = new Thread() {
-            public void run() {
-                try {
-                    String json = apiInterface.getUsers().execute().body().toString();
-                    Log.d("jsongetted", json);
-                    updateLV(json);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
+        UpdateThread t = new UpdateThread();
         t.start();
+    }
+
+    private class UpdateThread extends Thread {
+        @Override
+        public void run() {
+                    try {
+                        String json = apiInterface.getUsers().execute().body().toString();
+                        Log.d("jsongetted", json);
+                        updateLV(json);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+        }
     }
 
     private void updateLV(String json) {
@@ -89,12 +103,55 @@ public class MainActivity extends AppCompatActivity {
                 al );
 
         lv.setAdapter(arrayAdapter);
+        //lv.setClickable(true);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final TextView tv = (TextView) view.findViewById(R.id.listview_item_textview);
+
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Удалить?");
+                alertDialog.setMessage("Отправится DELETE запрос на удаление студента из базы данных на сервере." +
+                        "Данные о студенте: " + tv.getText().toString());
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Удалить",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                //Toast.makeText(getBaseContext(), tv.getText().toString(), Toast.LENGTH_SHORT).show();
+                                final String id = getIdFromString( tv.getText().toString());
+                                Toast.makeText(getBaseContext(), id, Toast.LENGTH_SHORT).show();
+
+
+                                Thread t = new Thread() {
+                                    public void run() {
+                                        Call<ResponseBody> deleteRequest = apiInterface.deleteUserByID(Integer.valueOf(id));
+                                        deleteRequest.enqueue(new Callback<ResponseBody>() {
+                                            @Override
+                                            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                                            }
+                                            @Override
+                                            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                                            }
+                                        });
+                                    }
+                                };
+                                t.start();
+
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
     }
 
     private View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
+                case R.id.button2_refresh:
+                    fastrestart();
+                    break;
                 case R.id.button_send:
                     Thread t = new Thread()  {
                         public void run() {
@@ -116,17 +173,53 @@ public class MainActivity extends AppCompatActivity {
                         }
                     };
                     t.start();
+                    break;
+                case R.id.button_change:
+                    Thread t2 = new Thread() {
+                        public void run() {
+                            User user = new User(
+                                    Integer.valueOf(et_id.getText().toString()),
+                                    et_students_name.getText().toString(),
+                                    et_students_phone.getText().toString());
+                            Call<User> call = apiInterface.updateUserByID(Integer.valueOf(et_id.getText().toString()), user);
+                            call.enqueue(new Callback<User>() {
+                                @Override
+                                public void onResponse(Call<User> call, Response<User> response) {
+                                    fastrestart();
+                                }
+                                @Override
+                                public void onFailure(Call<User> call, Throwable t) {
 
+                                }
+                            });
+                        }
+                    };
+                    t2.start();
                     break;
             }
         }
     };
 
+private String getIdFromString(String s) {
+    String id = "";
+    for (int i = 0; i < s.length() - 1; i++) {
+        if (s.charAt(i) == '='
+                && Character.isDigit(s.charAt(i + 1)) &&
+                s.charAt(i - 1) == 'd') {
+            id = Character.toString(s.charAt(i + 1));
+            if (s.charAt(i + 2) != '.'
+                    && Character.isDigit(s.charAt(i + 2))) {
+                id = id + Character.toString(s.charAt(i + 2));
+            }
+        }
+    }
+    return id;
+}
 
     private void fastrestart() {
-        Intent intent = getIntent();
-        finish();
-        startActivity(intent);
+        //Intent intent = getIntent();
+        //finish();
+       // startActivity(intent);
     }
     //private void doIngBackground() {
 
